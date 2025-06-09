093a2fc7fa7602fa1a7d6972bae5b0e3dbe26618
Girish Gupta <Girish_Gupta2@bmc.com>
2025-06-09 17:34:47 +0530

    updated attributions and a fix to provide right consul port

--- external/nginx/njs/audit.js
+++ external/nginx/njs/audit.js
@@ -1,3 +1,11 @@
+// *******************************************************************************
+//  Contributors: BMC Helix, Inc.
+//
+//  (c) Copyright 2020-2025 BMC Helix, Inc.
+//
+//* SPDX-License-Identifier: Apache-2.0
+// *******************************************************************************
+
 let fs = require('fs');
 
 const userIdHeader = process.env.UID_HTTP_HEADER || 'X-Credential-Identifier';
