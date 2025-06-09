d793fcd4a679a8925e7774a9ceb18fe5d594f49c
Girish Gupta <Girish_Gupta2@bmc.com>
2025-06-09 20:02:13 +0530

    updates to attribution  (#11)

--- external/nginx/njs/auth.js
+++ external/nginx/njs/auth.js
@@ -1,3 +1,11 @@
+// *******************************************************************************
+//  Contributors: BMC Helix, Inc.
+//
+//  (c) Copyright 2020-2025 BMC Helix, Inc.
+//
+//* SPDX-License-Identifier: Apache-2.0
+// *******************************************************************************
+
 let userSessionCache = new Map();
 
 const userIdHeader = process.env.UID_HTTP_HEADER || 'X-Credential-Identifier';
--- external/nginx/njs/env.js
+++ external/nginx/njs/env.js
@@ -1,3 +1,11 @@
+// *******************************************************************************
+//  Contributors: BMC Helix, Inc.
+//
+//  (c) Copyright 2020-2025 BMC Helix, Inc.
+//
+//* SPDX-License-Identifier: Apache-2.0
+// *******************************************************************************
+
 function getAuth(r) {
     let external_auth = process.env["IS_EXTERNAL_AUTH"];
     return external_auth !== undefined ? external_auth : "false";
